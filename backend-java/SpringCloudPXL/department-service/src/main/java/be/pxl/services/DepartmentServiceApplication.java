2646e73be206fea3d2a633fba7db049c34d51c34
Seppe Baerts <12202625@student.pxl.be>
2025-10-21 15:31:20 +0200

    fixed the build errors.

--- backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/DepartmentServiceApplication.java
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/DepartmentServiceApplication.java
@@ -1,11 +1,13 @@
 package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class DepartmentServiceApplication
 {
     public static void main( String[] args )
